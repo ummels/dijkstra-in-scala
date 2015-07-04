c0736af79952a44526310631ea8d9eb22f332432
Michael Ummels <michael@ummels.de>
2015-07-04 14:44:49 +0200

    Add some methods to Graph and SImpleGraph

--- src/main/scala/de/ummels/dijkstra/Graph.scala
+++ src/main/scala/de/ummels/dijkstra/Graph.scala
@@ -3,9 +3,23 @@ package de.ummels.dijkstra
 /**
  * A trait for weighted graphs.
  */
-trait Graph[N] {
+trait Graph[N] { self =>
   /** Returns a map with the successors of `n` and their costs. */
   def apply(n: N): Map[N, Int]
+
+  /** Returns a new graph with the given node `n` removed. */
+  def -(n1: N): Graph[N] = new Graph[N] {
+    def apply(n: N) =
+      if (n == n1) Map.empty
+      else self(n) - n1
+  }
+
+  /** Returns a new graph with the edge from `n1` to `n2` removed. */
+  def -(n1: N, n2: N): Graph[N] = new Graph[N] {
+    def apply(n: N) =
+      if (n == n1) self(n) - n2
+      else self(n)
+  }
 
   /** Performs a breadth-first search on this graph.
     *
